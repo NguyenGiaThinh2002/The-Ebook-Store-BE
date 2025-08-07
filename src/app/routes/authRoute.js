d17cf7d4781881ecdd8ee4f840a3e0bdb45cccdd
nmc9x <kysutudongcom@gmail.com>
2025-08-07 23:43:45 +0700

    --Updated and cleaned

--- src/app/routes/authRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const authController = require('../controllers/AuthController');
-
-const router = express.Router();
-
-router.post('/signup', authController.register);
-// router.use("/:slug", loginController.show)
-
-module.exports = router;
